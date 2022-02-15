5bd0606c9c57fe1da16c9643b88e04bc357cb407
Piyush Pipriye <61428343+piyushpipriye@users.noreply.github.com>
2022-02-15 13:15:09 +0530

    Add files via upload

---
+++ src/Chat.js
@@ -0,0 +1,101 @@
+import { Avatar, IconButton } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import './Chat.css'
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import SearchOutlined from '@mui/icons-material/SearchOutlined';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import { useParams } from 'react-router-dom';
+import Mic from '@mui/icons-material/Mic';
+import db from './firebase';
+import {useStateValue} from './StateProvider'
+import firebase from 'firebase/compat/app';
+
+function Chat() {
+    const [input, setInput] = useState("");
+    const [seed, setSeed] = useState('');
+    const { roomId } = useParams();
+    const [roomName, setRoomName] = useState('');
+    const [messages, setMessages] = useState([]);
+    const [{ user }, dispatch] =  useStateValue();
+
+    useEffect(() => {
+        if (roomId) {
+            db.collection('rooms')
+                .doc(roomId)
+                .onSnapshot((snapshot) => setRoomName
+                    (snapshot.data().name));
+
+            db.collection('rooms')
+                .doc(roomId)
+                .collection("messages")
+                .orderBy('timestamp', 'asc')
+                .onSnapshot((snapshot) =>
+                    setMessages(snapshot.docs.map((doc) =>
+                        doc.data()))
+                );
+        }
+    }, [roomId]);
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000))
+    }, []);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        db.collection('rooms').doc(roomId).collection
+        ('messages').add({
+            message: input,
+            name: user.displayName,
+            timestamp: firebase.firestore.FieldValue.
+            serverTimestamp(),
+        })
+        setInput('')
+    }
+
+    return (
+        <div className='chat'>
+            <div className='chat__header'>
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+                <div className='chat__headerInfo'>
+                    <h3>{roomName}</h3>
+                    <p>Last seen{" "}{new Date(messages[messages.length-1]?.timestamp?.toDate()).toUTCString()}</p>
+                </div>
+                <div className='chat__headerRight'>
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className='chat__body'>
+                {messages.map(message => (
+                    <p className={`chat__message ${
+                        message.name === user.displayName
+                        &&
+                        'chat__reciever'}`}>
+                        <span className='chat__name'>{message.name} </span>{message.message}
+                        <span className='chat__timestamp'>{new Date(message.timestamp?.toDate()).toUTCString()} </span>
+                    </p>
+                ))}
+            </div>
+
+            <div className='chat__footer'>
+                <InsertEmoticonIcon />
+                <form>
+                    <input value={input} onChange={(e) => setInput(e.target.value)} placeholder='Type a message' type="text" />
+                    <button onClick={sendMessage} type="submit">Send a message</button>
+                </form>
+                <Mic />
+            </div>
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQyqACHKfysblhwyT-BajSkjuLTQCQYwE",
+  authDomain: "whatsapp-b05fa.firebaseapp.com",
+  projectId: "whatsapp-b05fa",
+  storageBucket: "whatsapp-b05fa.appspot.com",
+  messagingSenderId: "108560375478",
+  appId: "1:108560375478:web:71f97bafe5cdd845cc9017",
+  measurementId: "G-TECNT8QZYD"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+export { auth, provider };
+export default db;
